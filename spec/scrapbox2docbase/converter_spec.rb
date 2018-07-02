26ea899415e4d62cf34dd4d23f87050ada23d50e
noriyotcp <noriyo.akita@gmail.com>
2018-07-02 23:47:24 +0900

    Add spec against Converter#convert!

--- spec/scrapbox2docbase/converter_spec.rb
+++ spec/scrapbox2docbase/converter_spec.rb
@@ -1,28 +1,64 @@
 RSpec.describe Scrapbox2docbase::Converter do
+  describe '#convert!' do
+    other = [
+      "[* 打ち消し線]",
+      " `[- 打ち消し]` ⇒ [- 打ち消し]",
+      " `[-***/ 斜体太字打ち消し]` ⇒ [-***/ 斜体太字打ち消し]",
+    ]
 
-  other = [
-    "[* 打ち消し線]",
-    " `[- 打ち消し]` ⇒ [- 打ち消し]",
-    " `[-***/ 斜体太字打ち消し]` ⇒ [-***/ 斜体太字打ち消し]",
-  ]
+    code_block = [
+      "code:サンプル.html",
+      " <div id=\"js-sample_wrapper\" class=\"videostream-HD\" align=\"center\">",
+      "     <video id=\"js-sample\" data-sample_video_id=\"sample\" class=\"video-js vjs-fluid vjs-default-skin vjs-big-play-centered\">",
+      "     </video>",
+      " </div>",
+    ]
 
-  code_blocks_one = [
-    "code:サンプル.html",
-    " <link href=\"https://scrapbox2docbase/test.min.css\" rel=\"stylesheet\">",
-    " <script src=\"https://scrapbox2docbase/test.min.js\"></script>",
-    " ",
-    " <div id=\"js-sample_wrapper\" class=\"videostream-HD\" align=\"center\">",
-    "     <video id=\"js-sample\" data-sample_video_id=\"sample\" class=\"video-js vjs-fluid vjs-default-skin vjs-big-play-centered\">",
-    "     </video>",
-    " </div>",
-    " <script type=\"text/javascript\">SamplePlayer('js-sample');</script>",
-  ]
+    table = [
+      "table:Sample table",
+      " Header1\tHeader2\tHeader3",
+      " Row1-Cell1\tRow1-Cell2\tRow1-Cell3",
+      " Row2-Cell1\tRow2-Cell2\tRow2-Cell3",
+      " Row3-Cell1\tRow3-Cell2\tRow3-Cell3",
+      "  This\tis\tthe end",
+    ]
 
-  code_blocks_two = [
-    "code:bash",
-    " TEST_ENV=test this is a test command",
-  ]
+    expected_other = [
+      "#### 打ち消し線",
+      "- `[- 打ち消し]` ⇒ ~~打ち消し~~",
+      "- `[-***/ 斜体太字打ち消し]` ⇒ ## ~~*斜体太字打ち消し*~~"
+    ]
 
-  let(:lines) { other + code_blocks_one + code_blocks_two }
-  # TODO: テスト書くこと
+    expected_code_block = [
+      "```サンプル.html",
+      "<div id=\"js-sample_wrapper\" class=\"videostream-HD\" align=\"center\">",
+      "    <video id=\"js-sample\" data-sample_video_id=\"sample\" class=\"video-js vjs-fluid vjs-default-skin vjs-big-play-centered\">",
+      "    </video>",
+      "</div>",
+      "```",
+    ]
+
+    expected_table = [
+      "- Sample table",
+      "",
+      "|Header1|Header2|Header3|",
+      "|---|---|---|",
+      "|Row1-Cell1|Row1-Cell2|Row1-Cell3|",
+      "|Row2-Cell1|Row2-Cell2|Row2-Cell3|",
+      "|Row3-Cell1|Row3-Cell2|Row3-Cell3|",
+      "| This|is|the end|"
+    ]
+
+    lines_json = other + code_block + table
+    lines_markdown = expected_other + expected_code_block + expected_table
+
+    before do
+      converter = Scrapbox2docbase::Converter.new(lines_json)
+      converter.convert!
+    end
+
+    it 'should convert JSON to Markdown' do
+      expect(lines_json).to eq lines_markdown
+    end
+  end
 end
