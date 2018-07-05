233ce6e8cddf1d6ebeb81379c0557a2bbfddfd7a
noriyotcp <noriyo.akita@gmail.com>
2018-07-05 20:20:42 +0900

    Generate YAML header to be embedded into the beginning of the Markdown file

--- lib/scrapbox2markdown/file_generator.rb
+++ lib/scrapbox2markdown/file_generator.rb
@@ -11,9 +11,10 @@ module Scrapbox2markdown
     def generate
       pages = import("#{@input}")
       pages.each do |page|
+        header = yaml_header(page)
         lines = page['lines'].map(&:chomp)
         Scrapbox2markdown::Converter.new(lines).convert!
-        export(lines: lines, filename: page['title'])
+        export(lines: lines.prepend(header), filename: page['title'])
       end
     end
 
@@ -34,6 +35,24 @@ module Scrapbox2markdown
       File.open("#{output_dir}/#{filename}.md", "w") do |io|
         lines.each { |line| io.puts(line) }
       end
+    end
+
+    # Markdownファイルの先頭に埋め込むため、YAML形式で出力する
+    # ---
+    # title: "はじめに"
+    # created_at: 2018-03-26 22:52:30 +0900
+    # updated_at: 2018-04-21 11:51:09 +0900
+    # ---
+    #
+    def yaml_header(page)
+      <<~YAML
+      ---
+      title: "#{page['title']}"
+      created_at: #{Time.at(page['created'])}
+      updated_at: #{Time.at(page['updated'])}
+      ---
+
+      YAML
     end
   end
 end
